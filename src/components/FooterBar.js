9c7efdcd039054adac664b749c1400aa004d29ce
Ismael Perez <ismaelperezemail@gmail.com>
2017-10-13 08:31:21 -0700

    minor style changes to user home page

--- src/components/FooterBar.js
+++ src/components/FooterBar.js
@@ -3,7 +3,9 @@ import React from 'react'
 const FooterBar = () => {
   return (
     <div className="footer">
-      © 2017 Time Capsule
+      <div className="footerText">
+        © 2017 Time Capsule
+      </div>
     </div>
   )
 }
